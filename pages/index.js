ccfe7246395375d9decb4ff7a767752b893a2827
Jean198 <jeandedieu.niyigaba@gmail.com>
2021-11-09 19:55:55 +0200

    first commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const index = () => {
+    return (
+        <div>
+            <h1>
+                Hello form next!
+            </h1>
+        </div>
+    )
+}
+
+export default index
